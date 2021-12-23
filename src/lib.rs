7c79f706cc37818bd6e7e92173531d622039c7db
Russkih Anatoly <titanofdarkness@gmail.com>
2021-12-23 16:45:48 +1000

    Various

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,34 @@
+extern crate reqwest;
+use reqwest::{Client, Response};
 use rocket::get;
 use std::collections::HashMap;
+use std::time::Duration;
+
+const AUTH_DOMAIN: &str = "https://auth.agilone.com/token";
+const MAX_TOKEN_AGE: u32 = 259200; // 3 days
+
+struct agileOneOperation {
+    create: bool,
+}
+
+async fn agil_one(operation_options: agileOneOperation) -> Response {
+    let client = Client::new();
+    let mut query = vec![("scheme", "a1webtag")];
+    let client = if operation_options.create {
+        query.push(("action", "create"));
+        client.post(AUTH_DOMAIN)
+    } else {
+        client.get(AUTH_DOMAIN)
+    };
+    let timeout = Duration::from_secs(5);
+    let resp = client
+        .basic_auth("username", Some("password"))
+        .query(&query)
+        .timeout(timeout)
+        .send()
+        .await;
+    resp.unwrap()
+}
 
 #[get("/")]
 pub async fn greeting() -> String {
